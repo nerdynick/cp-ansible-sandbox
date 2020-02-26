13643c91b5296864b003604fa25ecf700406cfe5
Nikoleta Verbeck <nerdynick@gmail.com>
2020-02-26 13:28:13 -0700

    Added EnvSetup Script

---
+++ env_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o nounset \
+    -o errexit \
+    -o verbose
+    
+PROJECT_DIR=$1
+
+#Make Project Directory and Change into it
+mkdir $PROJECT_DIR
+cd $PROJECT_DIR
+
+#Setup Project as a GIT repo
+git init
+
+#Checkout/Clone CP-ANSIBLE
+git submodule add https://github.com/confluentinc/cp-ansible.git
+
+#SymLink/Copy base into playbook scope
+ln -s cp-ansible/roles
+ln -s cp-ansible/tasks
+ln -s cp-ansible/filter_plugins
+cp cp-ansible/ansible.cfg ansible.cfg
+cp cp-ansible/all.yml install.yml
+
+#Add and make 1st commit
+git add *
+git commit -m"Initial Env Build Out"
