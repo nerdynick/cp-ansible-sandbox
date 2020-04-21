568af397eaa448f9ba4d5924957a170993b53712
Nikoleta Verbeck <nerdynick@gmail.com>
2020-04-21 14:39:01 -0600

    Updated MRC script

--- mrc_reconfig_topics.sh
+++ mrc_reconfig_topics.sh
@@ -25,3 +25,14 @@ cat << EOF >> generic-observers.json
     ]
 }
 EOF
+
+
+kafka-topics --bootstrap-server localhost:9092 --list | \
+    xargs -L1 kafka-configs --zookeeper zk1.dc1.nikoleta.aws.ps.confluent.io:2181 --entity-type topics --alter --replica-placement ~/generic-observers.json --entity-name
+    
+confluent-rebalancer execute \
+    --metrics-bootstrap-server localhost:9092 \
+    --bootstrap-server localhost:9092 \
+    --throttle 1000000 \
+    --zookeeper zk1.dc1.nikoleta.aws.ps.confluent.io:2181
+    # With 5.5 --replica-placement-only can be added
